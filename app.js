6790ef101eddebfe9c2242e17bb58a6931fb529e
Vesko Vujovic <veskovujovic@yahoo.com>
2015-06-03 15:56:20 +0200

    added some examples to jqlite
    added jqlite examples

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 //this must be global object
 var app = angular.module('myApp', []);
 
+//custom directive
+app.directive('mine', function(){
+
+	return {
+	   restrict: 'E',
+	   link: function(scope, element, attribute){
+	   	
+	   	element.addClass('box');
+	   	element.bind('click', function(e){
+          element.toggleClass('box');
+	   	});
+	    
+	    /* These are examples of selection children elements and applying some css style to them  
+	     * into angular.element() we must pass DOM element and then apply to them css 
+	     */
+	    angular.element(element.children()[0]).css({"background-color": "blue"});
+	    angular.element(element.children()[1]).css({"font-size": "46px"});
+
+
+	   },
+
+	   replace: true,
+	   templateUrl: 'directive.html'
+	};
+});
+/**
+  * One i mportant thing when we makedirective it supports only once root element
+  *
+  */
